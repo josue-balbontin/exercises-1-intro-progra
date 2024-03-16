cf06bffc0d883f39cd238657398ecde0942ac081
josue-balbontin <158528708+josue-balbontin@users.noreply.github.com>
2024-03-15 21:50:57 -0400

    prueba del ej 11

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -130,8 +130,12 @@ else {
 }
 
 string exercise_11(int number) {
-  // TODO: YOUR CODE HERE
-  return "";
+    if (number==11235813){
+      return "Se encontro Fibonacci";
+  }
+  else {
+      return "Esto no es de Fibonacci";
+  }
 }
 
 void exercise_12(string color1, int numb1,

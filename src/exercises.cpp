5bbe62a96e69a2ef5df69d747c588b3396ee6316
josue-balbontin <158528708+josue-balbontin@users.noreply.github.com>
2024-03-16 07:30:37 -0400

    prueba 1 ej 4

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -57,6 +57,15 @@ void exercise_3(int a, int b) {
 
 void exercise_4(double n, double a, double b, double x, double y) {
   // TODO: YOUR CODE HERE
+  if(n>a && n<b){
+    cout<<n-(n*x/100)<<endl;
+}
+else if (n>b){
+    cout<<n-(n*y/100)<<endl;
+}
+else{
+    cout<<n<<endl;
+}
 }
 
 void exercise_5(char character) {

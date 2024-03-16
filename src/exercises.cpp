414b7950e36e5f0854ef2c1f0feac2f564a0e456
josue-balbontin <158528708+josue-balbontin@users.noreply.github.com>
2024-03-16 11:42:35 -0400

    prueba 1 ej 15

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -231,6 +231,15 @@ else{
 
 void exercise_15(int a, int b, int c) {
   // TODO: YOUR CODE HERE
+      int d=a;
+    a=c;
+    c=b;
+    b=d;
+    
+    cout<<"Los valores son: a = "<<a<<" b = "<<b<<" y c = "<<c<<endl;
+    cout<<"Permutamos: a => b, b => c, c => a"<<endl;
+    cout<<"Los valores despues de la permutacion son: a = "<<a<<" b = "<<b<<" c = "<<c;
+
 }
 
 void exercise_16(int debut, int fin) {

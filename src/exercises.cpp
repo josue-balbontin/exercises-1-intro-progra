7f2257eedebe88ce76ebec48a37709498307cb85
josue-balbontin <158528708+josue-balbontin@users.noreply.github.com>
2024-03-16 09:15:12 -0400

    prueba 8 ej 13

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -201,7 +201,7 @@ void exercise_12(string color1, int numb1,
 string exercise_13(int age, int years_of_experience) {
   // TODO: YOUR CODE HERE
   if(age<18){
-    cout<<"Not eligible";
+    return "Not eligible";
 }
 else if (years_of_experience>=3 && years_of_experience<5){
     return "Project manager";
@@ -209,7 +209,7 @@ else if (years_of_experience>=3 && years_of_experience<5){
 else if (years_of_experience>=5){
     return "Senior project manager";
 }
-else if (years_of_experience<=3){
+else{
     return "Project coordinator";
 }
 }

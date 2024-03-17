3659d8992c9765157c756515650bdb2a34303e83
josue-balbontin <158528708+josue-balbontin@users.noreply.github.com>
2024-03-16 21:36:06 -0400

    prueba 2 ej 12

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -196,7 +196,12 @@ void exercise_12(string color1, int numb1,
                  string color3, int numb3,
                  string color4, int numb4) {
   // TODO: YOUR CODE HERE
-     if (color4==color3 || color4==color2 || color4==color1
+       if(color4==color3 && color4==color2 &&color4==color1){
+        if(numb4==numb3 && numb4==numb2 && numb4=numb1){
+            cout<<1<<endl<<2<<endl<<3<<endl;
+        }
+    }
+    else if (color4==color3 || color4==color2 || color4==color1
     ) { if(numb4 <0){
         if(numb3<numb2 && numb3<numb1){
             cout<<numb3<<endl;
@@ -214,12 +219,24 @@ void exercise_12(string color1, int numb1,
     
     }
     }
-    if (color4==color3 || color4==color2 || color4==color1){
+   else  if (color4==color3 || color4==color2 || color4==color1){
         if(numb4==numb3 || numb4==numb2 || numb4==numb1)
         {
             cout<<numb4<<endl;
         }
     }
+    
+
+
+
+
+
+
+
+
+
+
+
     
 }
 
